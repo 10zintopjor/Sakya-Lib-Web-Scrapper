e12ac3ca3838f77973f77f39713f28bd01756dd3
jungtop <tenzintopjor27@gmail.com>
2022-03-15 15:34:49 +0530

    added layers

--- parse.py
+++ parse.py
@@ -1,6 +1,16 @@
+from email.mime import base
+from fileinput import filename
 import re
 from traceback import print_tb
 from urllib import response
+from uuid import uuid4
+from openpecha.core.pecha import OpenPechaFS
+from openpecha.core.layer import InitialCreationEnum, Layer, LayerEnum,PechaMetaData
+from openpecha.core.annotation import Page, Span
+from openpecha.core.ids import get_pecha_id
+
+from datetime import datetime
+from venv import create
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -18,20 +28,18 @@ def get_text(book_id):
     page = BeautifulSoup(response.content,'html.parser')
     pagination_ul = page.select_one("ul.pagination li#pgNext")
     base_text = get_into_page(book_id)
-    with open("demo.txt","w") as f:
-        f.write(base_text)
+    return base_text
+    """ with open("demo.txt","w") as f:
+        f.write(base_text) """
 
-    
 
-def get_into_page(book_id,page_no=1):
+def get_into_page(book_id,page_no=1,base_text={}):
     response = make_request(text_api.format(book_id=book_id,page_no=page_no))
-    base_text=response.text
+    base_text.update({page_no:response.text.strip("\n")})
     if has_next_page(book_id,page_no+1):
-        base_text+=get_into_page(book_id,page_no+1)
-    else:
-        return base_text
-
-    return base_text    
+        _ = get_into_page(book_id,page_no+1,base_text)
+    return base_text
+    
 
 def has_next_page(book_id,page_no):
     status = ""
@@ -40,17 +48,76 @@ def has_next_page(book_id,page_no):
         status = False
     else:
         status = True     
-
     return status
+
 
 def extract_book_id(url):
     book_id = re.match(".*Book/(.*)",url).group(1)
     return book_id
 
+
+def create_opf(opf_path,base_text,filename):
+    opf = OpenPechaFS(opf_path=opf_path)
+    layers = {f"v{filename}": {LayerEnum.pagination: get_layers(base_text)}}
+    bases = {f"v{filename}":get_base_text(base_text)}
+    opf.layers = layers
+    opf.base = bases
+    opf.save_base()
+    opf.save_layers()
+
+
+def get_base_text(base_text):
+    text = ""
+
+    for elem in base_text:
+        text+=base_text[elem]+"\n\n"
+    return text
+
+
+def get_layers(base_text):
+    page_annotations= {}
+    char_walker = 0
+    for page_no in base_text:
+        page_annotation,char_walker = get_page_annotation(page_no,base_text[page_no],char_walker)
+        page_annotations.update(page_annotation)
+
+    pagination_layer = Layer(
+        annotation_type=LayerEnum.pagination,annotations=page_annotations
+    )    
+    return pagination_layer
+
+
+def get_page_annotation(page_no,text,char_walker):
+    page_start = char_walker
+    page_end = char_walker +len(text)
+    page_annotation = {
+        uuid4().hex:Page(span=Span(start=page_start,end=page_end),page_info=page_no)
+    }
+
+    return page_annotation,page_end+2
+
+
+def create_meta(opf_path):
+    instance_meta = PechaMetaData(
+        initial_creation_type=InitialCreationEnum.input,
+        created_at=datetime.now(),
+        last_modified_at=datetime.now(),
+        source_metadata="")
+    opf = OpenPechaFS(opf_path=opf_path)
+    opf._meta = instance_meta
+    opf.save_meta()
+
 def main():
     url = "http://sakyalibrary.com/library/Book/68d456e5-5314-4465-a02d-54a10c5b0adb"
+    filename = "demo_filename"
     book_id = extract_book_id(url)
-    get_text(book_id)
+    pecha_id = get_pecha_id()
+    opf_path = f"./opfs/{pecha_id}/{pecha_id}.opf"
+    base_text = get_text(book_id)
+    create_opf(opf_path,base_text,filename)
+    create_meta(opf_path)
+    
+
 
 if __name__ == "__main__":
     main()
