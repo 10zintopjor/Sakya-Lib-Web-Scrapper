4256e2e1e50f211340b85d8df623994bb284c09f
jungtop <tenzintopjor27@gmail.com>
2022-06-19 12:37:00 +0530

    added source

--- parse.py
+++ parse.py
@@ -4,7 +4,8 @@ from openpecha.core.metadata import InitialPechaMetadata,InitialCreationType
 from openpecha.core.annotation import Page, Span
 from openpecha.core.layer import Layer, LayerEnum
 
-from openpecha.core.ids import get_initial_pecha_id
+from openpecha.core.ids import get_initial_pecha_id,get_base_id
+from pathlib import Path
 from bs4 import BeautifulSoup
 from datetime import datetime
 import requests
@@ -15,6 +16,7 @@ from openpecha import github_utils,config
 
 input_source = "https://sakyalibrary.com"
 pechas_catalog = ''
+opf_root_path = "./opfs"
 err_log = ''
 text_api = 'http://sakyalibrary.com/library/BookPage?bookId={book_id}&pgNo={page_no}'
 main_api = 'http://sakyalibrary.com/library/Book/{book_id}'
@@ -24,21 +26,25 @@ def make_request(url):
     return response
 
 
-def get_text(book_id):
+def get_text(book_id,pecha_id):
     base_text = {}
     response = make_request(main_api.format(book_id=book_id))
-    page = BeautifulSoup(response.content,'html.parser')
-    base_text = get_into_page(book_id,1,base_text)
+    base_text = get_into_page(book_id,1,base_text,response.text.strip(),pecha_id)
     return base_text
 
 
-def get_into_page(book_id,page_no,base_text):
+def get_into_page(book_id,page_no,base_text,page_html,pecha_id):
     response = make_request(text_api.format(book_id=book_id,page_no=page_no))
+    save_source(page_html,page_no,pecha_id)
     base_text.update({page_no:response.text.strip("\n")})
     if has_next_page(book_id,page_no+1):
-        _ = get_into_page(book_id,page_no+1,base_text)
+        _ = get_into_page(book_id,page_no+1,base_text,response.text.strip())
     return base_text
-    
+
+def save_source(page_html,page_no,pecha_id):
+    source_file_path = Path(f"{opf_root_path}/{pecha_id}/Source") 
+    source_file_path.mkdir(parents=True, exist_ok=True)  
+    Path(source_file_path / f"PG{page_no}.html").write_text(page_html)  
 
 def has_next_page(book_id,page_no):
     status = ""
@@ -55,8 +61,8 @@ def extract_book_id(url):
     return book_id
 
 
-def create_opf(opf_path,base_text,base_id):
-    opf = OpenPechaFS(path=opf_path)
+def create_opf(opf_root_path,base_text,base_id):
+    opf = OpenPechaFS(path=opf_root_path)
     layers = {f"{base_id}": {LayerEnum.pagination: get_layers(base_text)}}
     bases = {f"{base_id}":get_base_text(base_text)}
     opf.layers = layers
@@ -95,17 +101,16 @@ def get_page_annotation(page_no,text,char_walker):
     return page_annotation,page_end+2
 
 
-def write_meta(opf_path,col):
+def write_meta(opf_root_path,col):
     instance_meta = InitialPechaMetadata(
         initial_creation_type=InitialCreationType.input,
         source=input_source,
         source_metadata={
             "title":col['title'],
             "parent":col["parent"],
-            "source":input_source,
             "bases":get_source_meta(col['vol'])
         })
-    opf = OpenPechaFS(opf_path=opf_path)
+    opf = OpenPechaFS(path=opf_root_path)
     opf._meta = instance_meta
     opf.save_meta()
 
@@ -130,21 +135,25 @@ def get_collections(url):
     page = BeautifulSoup(response.content,'html.parser')
     collections = page.select_one("div#tab_collections")
     divs = collections.findChildren("div",recursive=False)
-    for div in divs:
+    for div in reversed(divs):
         yield from get_links(div)
 
 
 def get_links(div):
-    main_title = div.select_one("h4.panel-title a span").text
+    print(div.select_one("h4.panel-title a span").text)
+    main_title = re.search("\s\D+",div.select_one("h4.panel-title a span").text)
+    main_title = main_title.group(0)
+    print(main_title)
     sub_titles = div.select("div.panel.panel-default.tab_topic")
-    for sub_title in sub_titles:
+    for sub_title in reversed(sub_titles):
         dict = {}
         vols =[]
-        link_tags = sub_title.find_next_sibling('div').select("div.file-text.row")
         has_more = sub_title.find_next_sibling('div').select_one("div.book-more a")
         if has_more:
             link = has_more["href"]
             link_tags = get_more_links(link,sub_title.text.strip())
+        else:
+            link_tags = sub_title.find_next_sibling('div').select("div.file-text.row")   
         for link_tag in link_tags:
             title = link_tag.select_one('div').text.strip()
             link = link_tag.select_one('a')['href']
@@ -179,7 +188,7 @@ def write_readme(pecha_id,col):
 def build(col):
     vols = col['vol']
     pecha_id = get_initial_pecha_id()
-    opf_path = f"./opfs/{pecha_id}/{pecha_id}.opf"
+    opf_root_path = f"./opfs/{pecha_id}/{pecha_id}.opf"
     base_id_title_map={}
     for vol in vols:
         if "/library/Book" not in vol['link']:
@@ -187,21 +196,15 @@ def build(col):
         base_id = get_base_id()
         base_id_title_map.update({base_id:[vol['title'],vol['author']]})
         book_id = extract_book_id("http://sakyalibrary.com"+vol['link']) 
-        base_text = get_text(book_id)
-        create_opf(opf_path,base_text,base_id)
-        print(col['title'])
+        base_text = get_text(book_id,pecha_id)
+        create_opf(opf_root_path,base_text,base_id)
         print(vol['title'])
     col["vol"] = base_id_title_map  
-    write_meta(opf_path,col)
+    write_meta(opf_root_path,col)
     write_readme(pecha_id,col)
-    publish_pecha(f"./opfs/{pecha_id}")
+    #publish_pecha(f"./opfs/{pecha_id}")
     pechas_catalog.info(f"{pecha_id},{col['title']}")
 
-def get_base_id():
-    id=uuid4().hex[:4]
-    while id.isnumeric() == True:
-        id = uuid4().hex[:4]
-    return id
 
 def set_up_logger(logger_name):
     logger = logging.getLogger(logger_name)
@@ -213,9 +216,9 @@ def set_up_logger(logger_name):
     return logger
 
 
-def publish_pecha(opf_path):
+def publish_pecha(opf_root_path):
     github_utils.github_publish(
-    opf_path,
+    opf_root_path,
     not_includes=[],
     message="initial commit"
     )
@@ -227,12 +230,13 @@ def main():
     pechas_catalog = set_up_logger("pechas_catalog")
     err_log = set_up_logger('err')
     for col in get_collections("http://sakyalibrary.com/library/collections"):
-        build(col)
+        if col["title"] == "Individual works":
+            build(col)
+            print("**********************")
         """ try:
             build(col)
         except:
             err_log.info(f"err :{col['title']}") """  
-        break
 
 def test_err():
     for col in get_collections("http://sakyalibrary.com/library/collections"):
